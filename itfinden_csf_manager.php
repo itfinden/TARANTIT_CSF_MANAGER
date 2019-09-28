3043d5088fbe535d6d8e11dffd8c770bee38c325
Friedrich <cm@itfinden.com>
2019-09-28 17:37:35 -0300

    fix mysqli

--- itfinden_csf_manager.php
+++ itfinden_csf_manager.php
@@ -34,7 +34,6 @@ function itfinden_csf_manager_activate()
 			PRIMARY KEY (`name`)
 		) ENGINE=MyISAM DEFAULT CHARSET=utf8";
 
-		echo $sql;
 	mysqli_query($sql);
 
 	$sql = "INSERT INTO `mod_csfmanager_config` (`name`, `value`) VALUES
