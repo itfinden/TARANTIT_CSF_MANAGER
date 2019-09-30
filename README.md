6856078be966b390900655c8425b2c3aa001e812
Man Of Orion <cm@itfinden.com>
2019-09-30 11:51:28 -0300

    clean

--- README.md
+++
@@ -1,5 +0,0 @@
-# ITFINDEN_CSF_MANAGER
-CSF MANAGER PARA WHMCCS
-
-se debe agregar a Modules / addons / la carpeta con el nombre itfinden_csf_manager
-
--- template/overall_footer.php
+++
@@ -1,5 +0,0 @@
-<?php if (!defined("ITFINDEN_CSF_MANAGER")) die("This file cannot be accessed directly"); ?>
-	</div>
-</div>
-
-<p style="text-align: center; margin-top: 10px;"><a Href="https://www.itfinden.com" target="itfinden">ITFINDEN</a> Plugin Version <?php echo $vars['version']; ?></p>
