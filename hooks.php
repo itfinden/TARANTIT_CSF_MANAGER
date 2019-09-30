a231ba06b4de2ad6b899b7cb69137308ab224676
Man Of Orion <cm@itfinden.com>
2019-09-30 13:52:43 -0300

    Update hooks.php

--- hooks.php
+++ hooks.php
@@ -1,9 +1,10 @@
 <?php
 
-if (!defined("WHMCS"))
+if (!defined("WHMCS")) {
 	die("This file cannot be accessed directly");
+}
 
-add_hook("DailyCronJob", 1, function() {
+add_hook("DailyCronJob", 1, function () {
 
 	$sql = "DELETE
 		FROM mod_csfmanager_allow
@@ -12,27 +13,27 @@ add_hook("DailyCronJob", 1, function() {
 
 });
 
-add_hook('ClientAreaPage', 1, function($vars) {
+add_hook('ClientAreaPage', 1, function ($vars) {
 
-	if($vars['action'] == 'productdetails' && $vars['serviceid'] && $vars['rawstatus'] == 'active')
-	{
-		require_once(dirname(__FILE__) . '/includes/functions.php');
+	if ($vars['action'] == 'productdetails' && $vars['serviceid'] && $vars['rawstatus'] == 'active') {
+		require_once (dirname(__FILE__) . '/includes/functions.php');
 
 		$instance = csfmanager::getInstance();
-		
+
 		$allowed_servers = explode(',', $instance->getConfig('servers'));
 
-		if(in_array($vars['serverdata']['id'], $allowed_servers))
-		{
+		if (in_array($vars['serverdata']['id'], $allowed_servers)) {
 			$menu = Menu::PrimarySidebar();
 
 			$overviewMenu = $menu->getChild('Service Details Actions');
 
 			$overviewMenu->addChild('Firewall', array(
-				'label' 	=> 'Manage Firewall',
-				'uri' 		=> 'index.php?m=csfmanager&id=' . $vars['serviceid'],
+				'label' => 'Manage Firewall',
+				'uri' => 'index.php?m=csfmanager&id=' . $vars['serviceid'],
 			));
 		}
+	} else {
+		echo 'algo paso';
 	}
 });
 
